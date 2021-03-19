60b273f2458c2c9ab6b4dbb7bdc491bd985228da
Mariyan Zhelyazkov <mariyanzhelyazkov924@gmail.com>
2021-03-19 16:04:37 +0200

    Mariyan Commit 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("helo");
         System.out.println("Hello from Mariyan");
-        System.out.println("H again.");
+        System.out.println("Hi again.");
     }
 }
